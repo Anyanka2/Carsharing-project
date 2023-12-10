519fa609baf05daec1ea908543fa9815a08dbd32
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-12-10 19:24:19 +0100

    added readme and adverts json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     
-      <BrowserRouter basename="carsharing-project">
+      <BrowserRouter basename="/carsharing-project">
         <App />
       </BrowserRouter>
     
